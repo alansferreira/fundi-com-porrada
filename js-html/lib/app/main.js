9f5117321decd95d2750fbdcc8c2dc2ea2b043ee
alan.da.silva.ferreira@hotmail.com <alan.da.silva.ferreira@hotmail.com>
2016-04-01 17:38:30 -0300

    first commit

---
+++ js-html/lib/app/main.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="../lib/requirejs/require.js" />
+/// <reference path="../deps/iioengine/core/iioEngine.min.js" />
+
+require(["iio"], function () {
+
+    iio.start(main);
+
+});
+
+function main(app, settings) {
+    // Create a loader with a base path at ".assets"
+    var loader = new iio.Loader('assets');
+
+    // Define assets in a hash, with each asset associated with a asset ID.
+    // in the form:
+    //   assetId: 'path/to/asset'
+    var assets = {
+        starSmall: 'images/starSmall.png',
+        starBig: 'images/starBig.png',
+        meteorBig: 'images/meteorBig.png',
+        meteorSmall: 'images/meteorSmall.png',
+        laserSound: 'sounds/laser.wav',
+        explode: 'sounds/explode.wav',
+        player: 'images/player.png',
+        playerLeft: 'images/playerleft.png',
+        playerRight: 'images/playerRight.png',
+        laser: 'images/laserRed.png',
+        laserFlash: 'images/laserRedShot.png',
+        theme: 'sounds/theme.mp3',
+    }
+    alert('asdf');
+
+}
---
+++ js-html/lib/require.config.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../lib/requirejs/require.js" />
+
+require.config({
+    baseUrl: "lib/",
+    paths: {
+        "iio": "deps/iioengine/core/iioEngine.min",
+        "main": "app/main"
+    },
+    waitSeconds: 15
+});
+
+requirejs(['app/main']);
